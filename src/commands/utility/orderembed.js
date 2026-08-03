a778964de702552cb8c5b7461efe8d6117df2178
Abdoo110 <abdooessied@gmail.com>
2026-08-03 22:39:44 +0200

    fix: both stickers as URL strings (Discord limit: 1 attachment per command)

--- src/commands/utility/orderembed.js
+++ src/commands/utility/orderembed.js
@@ -8,7 +8,7 @@ module.exports = {
     .setDescription('Create an order embed with stickers and an Order button')
     .addStringOption(o => o.setName('title').setDescription('Embed title').setRequired(true))
     .addStringOption(o => o.setName('description').setDescription('Embed description (single line)').setRequired(true))
-    .addAttachmentOption(o => o.setName('sticker').setDescription('Upload first sticker/image'))
+    .addStringOption(o => o.setName('sticker').setDescription('First sticker URL (right-click image → Copy Link)'))
     .addStringOption(o => o.setName('sticker2').setDescription('Second sticker URL (right-click image → Copy Link)'))
     .addChannelOption(o => o.setName('channel').setDescription('Channel to send to (default: current channel)'))
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageEvents)
@@ -17,8 +17,8 @@ module.exports = {
   async execute(interaction) {
     const title = interaction.options.getString('title');
     const desc = interaction.options.getString('description');
-    const sticker = interaction.options.getAttachment('sticker');
-    const sticker2Url = interaction.options.getString('sticker2');
+    const s1 = interaction.options.getString('sticker');
+    const s2 = interaction.options.getString('sticker2');
     const channel = interaction.options.getChannel('channel') || interaction.channel;
 
     const cfg = await getConfig(interaction.guild.id);
@@ -34,16 +34,9 @@ module.exports = {
     const row = new ActionRowBuilder().addComponents(button);
 
     const embeds = [];
-    if (sticker && sticker.contentType?.startsWith('image/')) {
-      embeds.push(new EmbedBuilder().setTitle(title).setDescription(desc).setImage(sticker.url).setColor('#5865F2'));
-    }
-    if (sticker2Url) {
-      embeds.push(new EmbedBuilder().setTitle(title).setDescription(desc).setImage(sticker2Url).setColor('#5865F2'));
-    }
-
-    if (embeds.length === 0) {
-      embeds.push(new EmbedBuilder().setTitle(title).setDescription(desc).setColor('#5865F2'));
-    }
+    if (s1) embeds.push(new EmbedBuilder().setTitle(title).setDescription(desc).setImage(s1).setColor('#5865F2'));
+    if (s2) embeds.push(new EmbedBuilder().setTitle(title).setDescription(desc).setImage(s2).setColor('#5865F2'));
+    if (embeds.length === 0) embeds.push(new EmbedBuilder().setTitle(title).setDescription(desc).setColor('#5865F2'));
 
     await channel.send({ embeds, components: [row] });
     await interaction.reply({ content: 'Order embed sent!', flags: MessageFlags.Ephemeral });
