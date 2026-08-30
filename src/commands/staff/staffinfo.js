7c48c78c601de573b5c52ddaffa5ab8c1fd991a5
Abdoo110 <abdooessied@gmail.com>
2026-08-31 00:29:28 +0200

    Fix staff registration and LOA status colors

--- src/commands/staff/staffinfo.js
+++ src/commands/staff/staffinfo.js
@@ -1,7 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { success, error } = require('../../utils/embed');
+const { success } = require('../../utils/embed');
 const Staff = require('../../models/Staff');
-const { buildEmbed } = require('../../utils/embed');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -12,26 +11,19 @@ module.exports = {
     .setDMPermission(false),
 
   async execute(interaction) {
-    const ign = interaction.options.getString('ign');
-    const timezone = interaction.options.getString('timezone');
+    const ign = interaction.options.getString('ign').trim();
+    const timezone = interaction.options.getString('timezone').trim();
 
-    let staff = await Staff.findOne({ guildId: interaction.guild.id, userId: interaction.user.id });
-    if (!staff) {
-      staff = await Staff.create({
-        guildId: interaction.guild.id,
-        userId: interaction.user.id,
-        ign,
-        timezone,
-        position: 'Staff',
-      });
-      await success(interaction, '📝 Staff Registered',
-        `**User:** ${interaction.user.tag}\n**IGN:** ${ign}\n**Timezone:** ${timezone}\n**Position:** Staff`);
-    } else {
-      staff.ign = ign;
-      staff.timezone = timezone;
-      await staff.save();
-      await success(interaction, '📝 Staff Info Updated',
-        `**User:** ${interaction.user.tag}\n**IGN:** ${ign}\n**Timezone:** ${timezone}\n**Position:** ${staff.position}`);
-    }
+    const staff = await Staff.findOneAndUpdate(
+      { guildId: interaction.guild.id, userId: interaction.user.id },
+      {
+        $set: { ign, timezone },
+        $setOnInsert: { position: 'Staff', hiredAt: new Date() },
+      },
+      { new: true, upsert: true, runValidators: true, setDefaultsOnInsert: true }
+    );
+
+    await success(interaction, '📝 Staff Info Saved',
+      '**User:** ' + interaction.user.tag + '\n**IGN:** ' + ign + '\n**Timezone:** ' + timezone + '\n**Position:** ' + staff.position);
   },
 };
